fc31e015cbd4611793ede483a0da2145d4d2ca0b
sagnik-rzt <sagnik.bhattacharya@razorthink.com>
2018-01-11 16:29:12 +0530

    Fixed some bugs

--- read_csv.py
+++ read_csv.py
@@ -22,28 +22,27 @@ def open_dataframes(source = "/home/sagnikb/PycharmProjects/Pyspark/*.csv123", o
             dict_keys = list(dict.keys())           #Gets the keys of the dictionary
             n_keys = len(dict_keys)
 
-            for key in range(n_keys):
-                key_column = df[dict_keys[key]]
-                worksheet.write(int(key), 0, dict_keys[key])
+            i = 0
+            for key in dict_keys:
+                key_column = df[key]
+                worksheet.write(i, 0, key_column.name)
 
                 unique_list = pd.DataFrame(data = key_column.unique())  #Prints the unique elements in each key
-                print(unique_list)
-                unique_list.to_excel(excel_writer = writer, sheet_name = 'Sheet3')
+                unique_list.to_excel(excel_writer = writer, sheet_name = 'Sheet3', startcol = 3*i + 1)
 
                 column_counts =  key_column.value_counts()
-                print(column_counts)
-                column_counts.to_excel(excel_writer = writer, sheet_name = 'Sheet4')        #Prints the count of elements under each key
+                column_counts.to_excel(excel_writer = writer, sheet_name = 'Sheet4', startcol = 3*i + 1)        #Prints the count of elements under each key
 
                 value_counts = key_column.value_counts()
-                print(value_counts)
-                value_counts.to_excel(excel_writer = writer, sheet_name = 'Sheet5')         #Prints the counts of each value that the elements under the key have
+                value_counts.to_excel(excel_writer = writer, sheet_name = 'Sheet5', startcol = 3*i + 1)         #Prints the counts of each value that the elements under the key have
 
                 NaNs = df.apply(lambda x: sum(x.isnull()), axis=0)
-                print(NaNs)
-                NaNs.to_excel(excel_writer = writer, sheet_name = 'Sheet6')    #Prints the number of null valued data-points in the dataset
+                NaNs.to_excel(excel_writer = writer, sheet_name = 'Sheet6', startcol = 3*i + 1)    #Prints the number of null valued data-points in the dataset
+                i += 1
 
             writer.save()
 
         file_count += 1
+
 
 open_dataframes()
