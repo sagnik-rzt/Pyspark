8170e3a5a6c22050142c7c806076765ffdb08261
sagnik-rzt <sagnik.bhattacharya@razorthink.com>
2018-01-11 22:07:02 +0530

    added number of unique elements per key

--- read_csv.py
+++ read_csv.py
@@ -4,19 +4,25 @@ import glob
 def open_dataframes(source = "/home/sagnikb/PycharmProjects/Pyspark/*.csv123", open_df = True):
 
     file_count = 0
-    for filename in glob.glob(source) :
+    writer = pd.ExcelWriter(str(file_count) + "excel_file.xlsx", engine='xlsxwriter')
+    workbook = writer.book
+    worksheet1 = workbook.add_worksheet('Sheet1')
 
-        writer = pd.ExcelWriter(str(file_count) + "excel_file.xlsx", engine='xlsxwriter')
+    for filename in glob.glob(source) :
         print(filename)
 
+        worksheet1.write(0, 2, "Filenames")
+        worksheet1.write(2, 2 + file_count, str(filename))
+
         if open_df == True :
+
             df = pd.read_csv(filepath_or_buffer = str(filename))   #Opens the CSV in pandas dataframe format
 
-            summary = df.describe()      #Statistical summary of numerical variables
-            summary.to_excel(excel_writer = writer, sheet_name = 'Sheet1')
-
-            workbook = writer.book
-            worksheet = workbook.add_worksheet('Sheet2')
+            worksheet2 = workbook.add_worksheet('Sheet2' + str(file_count))
+            worksheet2.write(0, 4, "Fraction of unique elements in given key")
+            worksheet2.write(0, 9, "Fraction of null elements")
+            worksheet2.write(0, 14, "Number of unique elements in given key")
+            worksheet2.write(0, 19, "Total number of elements in given key")
 
             dict = df.to_dict(orient = 'series')    #Gets the dictionary
             dict_keys = list(dict.keys())           #Gets the keys of the dictionary
@@ -25,34 +31,31 @@ def open_dataframes(source = "/home/sagnikb/PycharmProjects/Pyspark/*.csv123", o
             i = 0
             for key in dict_keys:
                 key_column = df[key]
-                worksheet.write(i, 0, key_column.name)
+                worksheet2.write(i + 1, 0, key_column.name)
                 n_elements = key_column.size
 
                 unique_list = key_column.unique()
                 n_unique_elements = len(unique_list)
                 unique_fraction = n_unique_elements/ n_elements
-                worksheet.write(i, 3, "Fraction of unique elements")
-                worksheet.write(i, 4, unique_fraction)   #Prints the fraction of unique elements in each key
 
-                unique_df = pd.DataFrame(data = unique_list)  #Prints the unique elements in each key
-                unique_df.to_excel(excel_writer = writer, sheet_name = 'Sheet3', startcol = (3*i + 1))
-
-                value_counts = key_column.value_counts()
-                value_counts.to_excel(excel_writer = writer, sheet_name = 'Sheet4', startcol = (3*i + 1))         #Prints the counts of each value that the elements under the key have
+                worksheet2.write(i + 1, 4, unique_fraction)   #Prints the fraction of unique elements in each key
 
                 n_NaNs = key_column.isnull().sum()
                 NaN_fraction = n_NaNs/n_elements
-                worksheet.write(i, 6, "Fraction of null elements")
-                worksheet.write(i, 7, NaN_fraction)
+                worksheet2.write(i + 1, 9, NaN_fraction)
+
+                worksheet2.write(i + 1, 14, n_unique_elements)
+                worksheet2.write(i + 1, 19, n_elements)
+
 
                 i += 1
-
-
 
             #Prints the number of null valued data-points in the dataset
 
             writer.save()
+
         file_count += 1
+
 
 
 open_dataframes()
