a3d5d7da9666d71ea1688c57977ebd9acba97f42
sagnik-rzt <sagnik.bhattacharya@razorthink.com>
2018-01-12 23:25:45 +0530

    added filesize and no of rows and columns

--- read_csv.py
+++ read_csv.py
@@ -1,28 +1,40 @@
 import pandas as pd
+import os
 import glob
 
-def open_dataframes(source = "/home/sagnikb/PycharmProjects/Pyspark/*.csv123", open_df = True):
+def open_dataframes(source = "*.csv123", open_df = True):
 
     writer = pd.ExcelWriter("excel_file.xlsx", engine='xlsxwriter')
     workbook = writer.book
     worksheet1 = workbook.add_worksheet('Sheet1')
+    worksheet1.write(0, 1, "Filenames")
+    worksheet1.write(0, 3, "Filesizes in bytes")
+    worksheet1.write(0, 5, "No. of rows")
+    worksheet1.write(0, 7, "No. of columns")
 
     file_count = 0
     for filename in glob.glob(source) :
         print(filename)
+        filesize = os.path.getsize(filename = str(filename))
 
-        worksheet1.write(0, 2, "Filenames")
-        worksheet1.write(2, 2 + file_count, str(filename))
+        worksheet1.write(1 + file_count, 1, str(filename))
+        worksheet1.write(1 + file_count, 3, filesize)
 
         if open_df == True :
 
             df = pd.read_csv(filepath_or_buffer = str(filename))   #Opens the CSV in pandas dataframe format
 
+            n_rows = df.count(axis = 0)[0]
+            n_columns = df.count(axis = 1)[0]
+
+            worksheet1.write(1 + file_count, 5, n_rows)
+            worksheet1.write(1 + file_count, 7, n_columns)
+
             worksheet2 = workbook.add_worksheet('Sheet2' + str(file_count))
-            worksheet2.write(0, 4, "Fraction of unique elements in given key")
-            worksheet2.write(0, 9, "Fraction of null elements")
-            worksheet2.write(0, 14, "Number of unique elements in given key")
-            worksheet2.write(0, 19, "Total number of elements in given key")
+            worksheet2.write(0, 3, "Fraction of unique elements in given key")
+            worksheet2.write(0, 5, "Fraction of null elements")
+            worksheet2.write(0, 7, "Number of unique elements in given key")
+            worksheet2.write(0, 9, "Total number of elements in given key")
 
             dict = df.to_dict(orient = 'series')    #Gets the dictionary
             dict_keys = list(dict.keys())           #Gets the keys of the dictionary
@@ -38,14 +50,14 @@ def open_dataframes(source = "/home/sagnikb/PycharmProjects/Pyspark/*.csv123", o
                 n_unique_elements = len(unique_list)
                 unique_fraction = n_unique_elements/ n_elements
 
-                worksheet2.write(i + 1, 4, unique_fraction)   #Prints the fraction of unique elements in each key
+                worksheet2.write(i + 1, 3, unique_fraction)   #Prints the fraction of unique elements in each key
 
                 n_NaNs = key_column.isnull().sum()
                 NaN_fraction = n_NaNs/n_elements
-                worksheet2.write(i + 1, 9, NaN_fraction)
+                worksheet2.write(i + 1, 5, NaN_fraction)
 
-                worksheet2.write(i + 1, 14, n_unique_elements)
-                worksheet2.write(i + 1, 19, n_elements)
+                worksheet2.write(i + 1, 7, n_unique_elements)
+                worksheet2.write(i + 1, 9, n_elements)
 
 
                 i += 1
