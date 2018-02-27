ab24e69fc6fafd796622e96026924763289ee2f2
sagnik-rzt <sagnik.bhattacharya@razorthink.com>
2018-02-27 18:30:16 +0530

    big brain script for savings data dictionary, minor edits

--- get_counts.py
+++ get_counts.py
@@ -19,15 +19,23 @@ folder_queue = generate_queue(source="/home/sagnikb/Downloads/Savings_data/SAMPL
 
 for _ in range(int(folder_queue.qsize())):
     foldername = folder_queue.get()
+    _, _, _, _, _, local_foldername = foldername.split("/")
+    print(foldername)
     filequeue = generate_queue(source=foldername + "/*.csv")
 
     for _ in range(int(filequeue.qsize())):
         filename = filequeue.get()
+        _, _, _, _, _, _, local_filename = filename.split("/")
+        print(filename)
         try:
             df = pd.read_csv(filepath_or_buffer=filename, usecols=['NATURE_QUERY', 'CLIENT_SUBFUNCTION'])
             for column in list(df.columns):
                 print(column)
-                print(df[column].value_counts(normalize=True))
+                counts = df[column].value_counts(normalize=True)
+                print(counts)
+                counts.to_csv(
+                    path_or_buf="/home/sagnikb/Downloads/Savings_data/" + column + local_filename + local_foldername + ".csv",
+                    sep=',')
                 print("\n")
 
 
@@ -38,5 +46,7 @@ for _ in range(int(folder_queue.qsize())):
             df = pd.read_csv(filepath_or_buffer=filename, usecols=['NATURE_QUERY'])
             for column in list(df.columns):
                 print(column)
-                print(df[column].value_counts(normalize=True))
-
+                counts = df[column].value_counts(normalize=True)
+                print(counts)
+                counts.to_csv(
+                    path_or_buf="/home/sagnikb/Downloads/Savings_data/" + column + local_filename + local_foldername + ".csv", sep=',')
